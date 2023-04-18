f142383a16772232a2395f8522a4636aad12891c
adbenitez <asieldbenitez@gmail.com>
2023-04-18 00:06:17 -0400

    add more tests

--- echobot_test.go
+++ echobot_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"testing"
 
+	"github.com/deltachat-bot/deltabot-cli-go/botcli"
 	"github.com/deltachat/deltachat-rpc-client-go/acfactory"
 	"github.com/stretchr/testify/assert"
 )
@@ -27,4 +28,41 @@ func TestEcho(t *testing.T) {
 	msg, err := acfactory.NextMsg(user)
 	assert.Nil(t, err)
 	assert.Equal(t, "hi", msg.Text)
+}
+
+func TestOnBotInit(t *testing.T) {
+	var err error
+
+	bot := acfactory.OnlineBot()
+	defer acfactory.StopRpc(bot)
+	user := acfactory.OnlineAccount()
+	defer acfactory.StopRpc(user)
+
+	onBotInit(nil, bot, nil, nil)
+	go bot.Run() //nolint:errcheck
+
+	chatWithBot, err := acfactory.CreateChat(user, bot.Account)
+	assert.Nil(t, err)
+
+	_, err = chatWithBot.SendText("hi")
+	assert.Nil(t, err)
+
+	msg, err := acfactory.NextMsg(user)
+	assert.Nil(t, err)
+	assert.Equal(t, "hi", msg.Text)
+}
+
+func TestOnBotStart(t *testing.T) {
+	bot := acfactory.OnlineBot()
+	defer acfactory.StopRpc(bot)
+
+	cli := botcli.New("testbot")
+	onBotStart(cli, bot, nil, nil)
+}
+
+func TestOnInfoCmd(t *testing.T) {
+	bot := acfactory.OnlineBot()
+	defer acfactory.StopRpc(bot)
+
+	onInfoCmd(nil, bot, nil, nil)
 }
