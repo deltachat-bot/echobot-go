8e0189159cf8a0545b625a42d00fa9e4818047d7
adbenitez <asieldbenitez@gmail.com>
2023-04-17 23:51:00 -0400

    update to API changes in deltabot-cli-go v0.4.1-0.20230418033838-9b3aebb62d58

--- echobot.go
+++ echobot.go
@@ -8,31 +8,33 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var cli *botcli.BotCli
-
 // echo back received text
-func echo(msg *deltachat.Message) {
+func echo(bot *deltachat.Bot, msg *deltachat.Message) {
 	snapshot, _ := msg.Snapshot()
-	chat := &deltachat.Chat{Account: snapshot.Account, Id: snapshot.ChatId}
+	chat := &deltachat.Chat{Account: msg.Account, Id: snapshot.ChatId}
 	if snapshot.Text != "" { // ignore messages without text
 		chat.SendText(snapshot.Text) //nolint:errcheck
 	}
 }
 
-func onInfoCmd(bot *deltachat.Bot, cmd *cobra.Command, args []string) {
+func onInfoCmd(cli *botcli.BotCli, bot *deltachat.Bot, cmd *cobra.Command, args []string) {
 	sysinfo, _ := bot.Account.Manager.SystemInfo()
 	for key, val := range sysinfo {
 		fmt.Printf("%v=%#v\n", key, val)
 	}
 }
 
-func onBotStart(bot *deltachat.Bot, cmd *cobra.Command, args []string) {
+func onBotInit(cli *botcli.BotCli, bot *deltachat.Bot, cmd *cobra.Command, args []string) {
+	bot.OnNewMsg(echo)
+}
+
+func onBotStart(cli *botcli.BotCli, bot *deltachat.Bot, cmd *cobra.Command, args []string) {
 	addr, _ := bot.GetConfig("configured_addr")
 	cli.Logger.Infof("Listening at: %v", addr)
 }
 
 func main() {
-	cli = botcli.New("echobot")
+	cli := botcli.New("echobot")
 
 	// add an "info" CLI subcommand as example
 	infoCmd := &cobra.Command{
@@ -42,7 +44,7 @@ func main() {
 	}
 	cli.AddCommand(infoCmd, onInfoCmd)
 
-	cli.OnBotInit(func(bot *deltachat.Bot, cmd *cobra.Command, args []string) { bot.OnNewMsg(echo) })
+	cli.OnBotInit(onBotInit)
 	cli.OnBotStart(onBotStart)
 
 	err := cli.Start()
--- go.mod
+++ go.mod
@@ -4,8 +4,8 @@ module github.com/deltachat-bot/echobot-go
 go 1.20
 
 require (
-	github.com/deltachat-bot/deltabot-cli-go v0.4.0
-	github.com/deltachat/deltachat-rpc-client-go v0.17.1-0.20230417205838-af6c36ff391b
+	github.com/deltachat-bot/deltabot-cli-go v0.4.1-0.20230418033838-9b3aebb62d58
+	github.com/deltachat/deltachat-rpc-client-go v0.17.1-0.20230417222922-fd102c51053c
 	github.com/spf13/cobra v1.7.0
 	github.com/stretchr/testify v1.8.2
 )
